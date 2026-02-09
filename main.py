ab8c42f16165d94e54958615dcbea902c87a4657
Gokulnath.G <814725243047@trp.srmtrichy.edu.in>
2026-02-09 14:31:02 +0530

    Add Student class with display method

---
+++ main.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print("name =", self.name)
+        print("marks =", self.marks)
+
+
+s1 = Student("gokul", 95)
+s1.display()
